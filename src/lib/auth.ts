6026da78093d2bfe07308248ffdbe8d5cbd6dcaf
kev <kquach6@wgu.edu>
2025-08-29 09:08:47 -0700

    frontend

---
+++ src/lib/auth.ts
@@ -0,0 +1,11 @@
+import { compare, hash } from "bcryptjs";
+
+
+
+export async function hashPassword(password: string) {
+    return hash(password, 12);
+}
+
+export async function comparePasswords(plainPassword: string, hashedPassword: string) {
+    return compare(plainPassword, hashedPassword)
+}
